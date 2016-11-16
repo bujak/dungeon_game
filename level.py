ea51a754503930a006c87b7cbb640c7bbaa68124
bujak <bujakowski@gmail.com>
2016-11-17 00:22:01 +0100

    levels almost works as i want

--- level.py
+++ level.py
@@ -11,7 +11,7 @@ def gameboard(x=30, y=90):
         tab.append([])
         for kolumn in range(y):
             if rzad == 0 or rzad == x-1 or kolumn == 0 or kolumn == y-1: 
-                tab[rzad].append('x')
+                tab[rzad].append('#')
             else:
                 tab[rzad].append('.')
     return tab
@@ -122,8 +122,16 @@ def positioning_escape(gameboard):
 
     return gameboard
 
-def escape():
+def coloring_list(): #coloring printed gameboard with random color
+    color_list = ["'\033[95m'", "'\033[94m'", "'\033[92m'", "'\033[93m'",
+                  "'\033[91m'", "'\033[0m'", "\[\033[0;32m\]", "\[\033[0;35m\]",
+                  "\[\033[0;35m\]", "\[\033[0;36m\]"]
+    rand_color = random.choice(color_list)
+    print(rand_color)
+
+def escape(tab, game):
     esc = find("E")
+
     player = find("@")
     f=None
     print(esc)
@@ -134,18 +142,49 @@ def escape():
         file_contents = f.read()
         print(file_contents)
         f.close()
-        time.sleep(3)
+        time.sleep(1)
         os.system("clear")
-        tab[rows-2][columns//2]= "@"
+        tab[28][45] = "@"
+        player_x = player[0]
+        player_y = player[1]
+        tab[player_x][player_y]="."
         positioning_escape(tab)
+        # tab = gameboard()
+        coloring_list()
+        create_box()
 
-tab = gameboard()
-settable(tab)
-create_box()
-put_box(20)
-positioning_escape(tab)
-while True:
-    escape()
-    printing_gameboard(tab)
-    key = getch()
-    move(key)
+        put_box(10)
+
+
+
+
+
+def new_lvl(tab):
+
+    tab = gameboard()
+    settable(tab)
+    create_box()
+    put_box(2)
+    positioning_escape(tab)
+    while True:
+        escape(tab)
+        printing_gameboard(tab)
+        key = getch()
+        move(key)
+    return tab
+
+if __name__ == '__main__':
+
+    tab = gameboard()
+    settable(tab)
+    create_box()
+    put_box(10)
+    positioning_escape(tab)
+    game = 0
+    while game == 0:
+        printing_gameboard(tab)
+        key = getch()
+        move(key)
+        escape(tab, game)
+
+
